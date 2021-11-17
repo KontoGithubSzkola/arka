af1f7e1b859656c4008297e3dd48d1636c23145a
student <julian.stajnke01@gmail.com>
2021-11-17 08:43:15 +0100

    chuj

---
+++ app/src/main/java/com/example/myapplicatione/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplicatione
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            findViewById<Button>(R.id.button).setOnClickListener{
+                findViewById<EditText>(R.id.editTextTextMultiLine).setText("Julian \n Stajnke")
+
+            }
+
+    }
+}
